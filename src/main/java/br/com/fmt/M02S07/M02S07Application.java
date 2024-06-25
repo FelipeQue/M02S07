502de54fa976f559f9826046b29334c6a01534f3
Felipe Quérette <felipe.querette@gmail.com>
2024-06-24 21:51:27 -0300

    first commit

---
+++ src/main/java/br/com/fmt/M02S07/M02S07Application.java
@@ -0,0 +1,13 @@
+package br.com.fmt.M02S07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M02S07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M02S07Application.class, args);
+	}
+
+}
