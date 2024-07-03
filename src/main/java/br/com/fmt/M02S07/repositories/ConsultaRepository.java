b5f63213f0fd5cbb7527a50662de3abc766bc655
Felipe Quérette <felipe.querette@gmail.com>
2024-07-03 16:50:30 -0300

    add @repository annotation to repositories

--- src/main/java/br/com/fmt/M02S07/repositories/ConsultaRepository.java
+++ src/main/java/br/com/fmt/M02S07/repositories/ConsultaRepository.java
@@ -2,6 +2,8 @@ package br.com.fmt.M02S07.repositories;
 
 import br.com.fmt.M02S07.entities.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConsultaRepository extends JpaRepository<Consulta,Long> {
 }
--- src/main/java/br/com/fmt/M02S07/repositories/PacienteRepository.java
+++ src/main/java/br/com/fmt/M02S07/repositories/PacienteRepository.java
@@ -2,6 +2,8 @@ package br.com.fmt.M02S07.repositories;
 
 import br.com.fmt.M02S07.entities.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PacienteRepository extends JpaRepository<Paciente,Long> {
 }
